2a8edfa0adffa7a167e0b8160ddfc29143edb4f4
ricardogsdev <ricardogsdev@gmail.com>
2021-11-21 23:00:40 -0300

    Consumindo Api e construindo o Modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
+import { useState } from "react";
+import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement('#root');
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsnreTransactionModalOpen] = useState(false);
+
+    function handleOpenNewTransactionModal(){
+        setIsnreTransactionModalOpen(true);
+    }
+
+    function handleCloseNewTransactionModal(){
+        setIsnreTransactionModalOpen(false);
+    }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen} 
+        onRequestClose={handleCloseNewTransactionModal} 
+      />
+          
       <GlobalStyle />
     </>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom';
 import { createServer } from 'miragejs';
 import { App } from './App';
 
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Foof',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
